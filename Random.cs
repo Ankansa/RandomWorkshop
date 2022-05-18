d459f2d6bf23dcdd75f8e4eecceb5a7ed804a605
ankan <ankan@DESKTOP-0OQR61J>
2022-05-18 09:36:44 +0530

    Add project files.

---
+++ Random.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomWorkshop
+{
+    internal class Randome
+    {
+        public static void Randomemethod()
+        {
+            Random obj1 = new Random();
+            int num1 = obj1.Next(1, 7);
+            Console.WriteLine(num1);
+
+        }
+        
+
+    }
+}
